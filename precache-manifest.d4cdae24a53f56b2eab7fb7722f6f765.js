5da6269f59818693c10a2a47836a02ec96456f1c
Emanuel Torres <emanueljtc@gmail.com>
2020-10-23 17:24:37 -0400

    finish build

---
+++ precache-manifest.d4cdae24a53f56b2eab7fb7722f6f765.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b60592cd3cdc871ae0c4348e938fb89",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c545216f2475564507d8",
+    "url": "/static/css/main.016a7ec3.chunk.css"
+  },
+  {
+    "revision": "848479bf50b70a83ab8e",
+    "url": "/static/js/2.bb9dc33f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.bb9dc33f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c545216f2475564507d8",
+    "url": "/static/js/main.272ebfb7.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
